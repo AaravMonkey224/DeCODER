c3bf332f20e7dbd92eea95e6f080d198925fbdac
Aarav Shah <60462261+AaravMonkey224@users.noreply.github.com>
2021-08-06 09:58:16 +0000

    Completed encryption prog, started decryption

--- main.py
+++ main.py
@@ -1,33 +1,57 @@
+# imports
 import os
 import screens
-from colorama import Fore as colour
-from subprocess import call
+import hashlib
+from colorama import Fore as colour, Back, Style
 from time import sleep as wait
 
+# definitions
+from subprocess import call
 
 def clear():
     _ = call('clear' if os.name =='posix' else 'cls')
 
 
+# screen functions
+def encryptionProg():
+	print(screens.encryptionScreen)
+	encyr_input = input(": " + colour.GREEN)
+
+	if encyr_input == "~":
+		clear()
+		main()
+	else:
+		result = hashlib.md5(encyr_input.encode())
+
+		print(colour.WHITE + "\n The encrypted string is" + colour.CYAN + ": " + colour.RED + str(result.digest()))
+		input(colour.BLACK + Back.WHITE + Style.BRIGHT + "\n[PRESS ENTER TO CONTINUE]	" + Style.RESET_ALL)
+
+		clear()
+		encryptionProg()
+		
 def main():
 	start = True
 	while start:
+		print(screens.title)
 		print(screens.titleScreen)
-		choice = input(": ")
+		choice = input(": " + colour.GREEN)
+
 		if choice == "1":
 			start = False
 			clear()
-			print("Working on it..")
+			encryptionProg()
+		elif
 		elif choice == "~":
 			start = False
 			clear()
 			wait(0.07)
-			print(screens.title)
+			print("")
+			print(colour.RED + "YOU ARE ALREADY AT THE HOME PAGE\n" + colour.WHITE)
 			main()
 		else:
 			clear()
-			print(colour.RED + "THAT WAS NOT A VALID INPUT" + colour.WHITE)
+			print("")
+			print(colour.RED + "THAT WAS NOT A VALID INPUT\n" + colour.WHITE)
 
 if __name__ == "__main__":
-	print(screens.title)
 	main()
--- screens.py
+++ screens.py
@@ -1,8 +1,9 @@
-title = "		- DeCODER - "
-titleScreen = """
-press '1' for encrypting.
-more options comming soon
+from colorama import Fore as colour, Back, Style
 
-(you can tyoe '~' at any time to get back to the title page)
-"""
+title = (colour.CYAN + Style.BRIGHT +"		- DeCODER - " + colour.WHITE + Style.NORMAL)
 
+titleScreen = ("\n- press '" + colour.BLUE + "1" + colour.WHITE + "' for encrypting.\n- press '"+ colour.BLUE + "2" + colour.WHITE + "' for decrypting\n\nmore options comming soon\n(you can type '" + colour.BLUE + "~" + colour.WHITE + "' at any time to get back to the title page)\n")
+
+
+
+encryptionScreen = (colour.CYAN + Style.BRIGHT + "		- ENCRYPTION - \n\n" + Style.NORMAL + colour.WHITE + "type the phrase you want to encrypt or type '" + colour.BLUE + "~" + colour.WHITE + "' to return to the title screen\n\n")
